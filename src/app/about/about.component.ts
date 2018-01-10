06f073195903cd00aafbe93a1365ea5db1fca844
Vishesh <visheshsingh16@gmail.com>
2018-01-10 09:55:21 -0600

    Setting and fetching router parameters

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routes: ActivatedRoute) { 
+    this.routes.params.subscribe(res => console.log(res.id));
+  }
 
   ngOnInit() {
   }
