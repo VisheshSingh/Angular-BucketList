6896b6b1dc95a8e0fa209a949ccfd84a25becd6e
Vishesh <visheshsingh16@gmail.com>
2018-01-10 10:22:14 -0600

    Services added to share the data between the components

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -9,11 +10,13 @@ import { Router } from '@angular/router';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private routes: ActivatedRoute, private router: Router) { 
+  goals: any;
+  constructor(private routes: ActivatedRoute, private router: Router, private _data: DataService) { 
     this.routes.params.subscribe(res => console.log(res.id));
   }
 
   ngOnInit() {
+    this._data.goal.subscribe(res => this.goals = res);
   }
 
   sendMeHome() {
